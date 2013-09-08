67e200566d825099e2547aa2356bafa838ba1f10
Ramon Nogueira <ramon@echophase.com>
2013-09-08 01:22:40 +0100

    Formatting

--- test/test_api_multiprocess.rb
+++ test/test_api_multiprocess.rb
@@ -5,7 +5,7 @@ require 'pry'
 
 class TestApiMultiprocess < MiniTest::Unit::TestCase
 
-	def test_approx_distinct
+  def test_approx_distinct
     @counter = 'approx_distinct'
     @row = 'abc'
     @column = 'def'
@@ -17,37 +17,37 @@ class TestApiMultiprocess < MiniTest::Unit::TestCase
       end
     end
 
-		post_sequence_on('8002', 1, 1000)
-		result = read_count_from('8003')
-		assert_in_epsilon 1000, result, 0.1
+    post_sequence_on('8002', 1, 1000)
+    result = read_count_from('8003')
+    assert_in_epsilon 1000, result, 0.1
 
     post_sequence_on('8004', 1001, 5000)
-		
-		result = read_count_from('8003')
-		assert_in_epsilon 5000, result, 0.1
-		
-		result = read_count_from('8005')
-		assert_in_epsilon 5000, result, 0.1
 
-		all_instances do |port|
-			post_sequence_on(port, 1, 5000)
-		end
-		result = read_count_from('8006')
-		assert_in_epsilon 5000, result, 0.1
-	end
+    result = read_count_from('8003')
+    assert_in_epsilon 5000, result, 0.1
 
-	def all_instances 
-		(8001..8009).each do |n|
-			yield n
-		end
-	end
+    result = read_count_from('8005')
+    assert_in_epsilon 5000, result, 0.1
 
-	def post_sequence_on(port, range_start, range_end)
+    all_instances do |port|
+      post_sequence_on(port, 1, 5000)
+    end
+    result = read_count_from('8006')
+    assert_in_epsilon 5000, result, 0.1
+  end
+
+  def all_instances
+    (8001..8009).each do |n|
+      yield n
+    end
+  end
+
+  def post_sequence_on(port, range_start, range_end)
     uri = URI("http://localhost:#{port}/#{@counter}/#{@row}/#{@column}/add-sequence")
     Net::HTTP.post_form(uri, range_start: range_start, range_end: range_end)
   end
 
-	def read_count_from(port)
+  def read_count_from(port)
     uri = URI("http://localhost:#{port}/#{@counter}/#{@row}")
     response = Net::HTTP.get(uri)
     JSON.parse(response)[@column]
