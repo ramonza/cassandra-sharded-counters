b8814799c98ef45a78ff972ecf5b1b6898617b36
Ramon Nogueira <ramon@echophase.com>
2013-09-08 01:11:00 +0100

    Fixed doc

--- lib/aggregate_table.rb
+++ lib/aggregate_table.rb
@@ -5,17 +5,7 @@ require 'forwardable'
 require 'mutator'
 require 'garbage_collector'
 
-# Wraps an aggregate table in Cassandra. The table is logically keyed by (+row_key+, +column_key+). There are
-# three additional components of the primary key used to separate the counters into locally consistent shards:
-# 1. +host_id+ - opaque integer value identifying an distinct entity or 0
-# 2. +run+ - monotonically increasing (per host_id) sequence or 0
-# 3. +generation+ - opaque integer value or 0
-# For an entry <tt>(host_id, run, generation, final, current_state)</tt>, +current_state+ is _final_ if
-# 1. The entry <tt>(host_id, current_run)</tt> appears in the +host_current_runs+ table, where <tt>current_run > run</tt>
-# 2. +final+ has the value +true+
-# Final values will never be updated so it is safe for any actor who determines that a state is final to merge
-# all the final states for a given <tt>(row_key, column_key)</tt> by inserting the merged value and deleting the old values
-# atomically (they should have the same partition key).
+# Wraps an aggregate table in Cassandra. Each (+row_key+, +column_key+) hosts a single counter.
 class AggregateTable
 
   include CqlHelper
